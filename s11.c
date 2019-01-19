c548fed1b542183f33b14e452bd76008b3513b77
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-01-19 14:46:04 +0530

    Create s11.c

---
+++ s11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the number is positive");
+}
+else if(a==0)
+{
+printf("the number is zero");
+}
+else
+{
+printf("the number is negative");
+}
+getch();
+}
