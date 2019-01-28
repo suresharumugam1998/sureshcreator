80de26451d2425b8f91506ffa652744609722252
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-01-28 15:25:54 +0530

    Create 1oddeve2.c

---
+++ 1oddeve2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+getch();
