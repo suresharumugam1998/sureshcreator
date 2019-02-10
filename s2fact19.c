c5584634ddc53ac8dc4426f5e1235783f6ab706c
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-10 20:46:14 +0530

    Create s2fact19.c

---
+++ s2fact19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,fact=1;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        
+        fact=fact*i;
+    }
+        printf("%d",fact);
+    getch();
