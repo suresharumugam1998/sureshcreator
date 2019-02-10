e9f98d38876d33335c0e7d37d621c832d9be0041
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-11 00:00:56 +0530

    Create s3quick6.c

---
+++ s3quick6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main()
+{
+  int n,i,j,a[100],temp;
+  printf("enter the number for quick sort");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])
+      {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+  for(i=0;i<n;i++)
+  {
+    printf("%d\n",a[i]);
+  }
+  
+}
