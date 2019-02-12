b5dfcb5df8c99d02d00eaa0ef9d470f1de4f7e80
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 19:02:08 +0530

    Create s5counum5.c

---
+++ s5counum5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char s[50];
+	int i,count=0;
+	printf("enter the string:");
+	scanf("%s",s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]>='0' && s[i]<='9')
+		{
+			count=count+1;
+		}
+	}
+	printf("\n%d",count);
+	
+}
