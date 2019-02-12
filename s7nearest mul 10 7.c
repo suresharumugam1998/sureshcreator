f1454797a6ae10b6d020f6c512ed11859a411dc3
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 22:06:31 +0530

    Create s7nearest mul 10 7.c

---
+++ s7nearest mul 10 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	int num;
+	printf("enter the number:");
+	scanf("%d",&num);
+	num+=1;
+	while(num%10!=0)
+	{
+		num=num+1;
+	}
+	printf("\n%d",num);
+	
+}
