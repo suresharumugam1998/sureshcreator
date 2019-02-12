ab2b25ce186690c9416ec07580bbe8a571dd52be
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 19:25:31 +0530

    Create s6neareve4.c

---
+++ s6neareve4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+void main() 
+{
+	int num,even;
+	printf("enter the number:");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		even=num-2;	
+	}
+	else
+	{
+		even=num-1;
+	}
+	printf("\n%d",even);
+	
+}
