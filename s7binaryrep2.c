a9d420cca952919b5fcb63d46fd0fe56877692b0
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 21:44:02 +0530

    Create s7binaryrep2.c

---
+++ s7binaryrep2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+
+{
+
+	char a[10];
+
+	scanf("%s",a);
+
+
+
+	int i,flag=0,m;
+
+	m=strlen(a);
+
+	for(i=0;i<m;i++)
+
+	{
+
+		if(a[i]=='1' || a[i]=='0')
+
+		{
+
+			flag=1;
+
+		}
+
+		else
+
+		{
+
+			flag=0;
+
+			break;
+
+		}
+
+		
+
+	}
+
+	if(flag==1)
+
+	{
+
+		printf("yes");
+
+	}
+
+	
+
+}
