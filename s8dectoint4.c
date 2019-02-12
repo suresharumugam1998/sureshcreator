319f75cb5f7c20398b83ada25e4dea37c72e83fe
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 22:30:26 +0530

    Create s8dectoint4.c

---
+++ s8dectoint4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+void main() 
+{
+	float num;
+	int x;
+	printf("\n enter the num:");
+	scanf("%f",&num);
+	x=(int)(num+0.5);
+	printf("\n%d",x);
+	
+}
