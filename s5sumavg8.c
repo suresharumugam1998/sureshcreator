ef117ee5ce14630be0f0c5b89a93ac420529c7c2
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-12 19:08:27 +0530

    Create s5sumavg8.c

---
+++ s5sumavg8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,n,array[50],sum=0,avg;
+	printf("enter the size of the array");
+	scanf("%d",&n);
+	printf("enter the array elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+array[i];
+	}
+	avg=sum/n;
+	printf("\n%d",sum);
+	printf("\n%d",avg);
+	
+}
