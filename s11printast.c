432a3eabe7c29303631ea5d4c6a6475029f30f2f
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-21 14:25:36 +0530

    Create s11printast.c

---
+++ s11printast.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char str[50];
+	int i,n;
+	scanf("%s %d",str,&n);
+	int len;
+	len=strlen(str);
+	for(i=n;i<=len;i++)
+	{
+		printf("%c",str[i]);
+	}
+
+
+}
