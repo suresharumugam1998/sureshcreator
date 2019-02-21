458770af1d79763b719a5d59f8684a6214a1e08f
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-21 15:23:29 +0530

    Create ps1printfweekend yes or no.c

---
+++ ps1printfweekend yes or no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+int i,j;
+printf("enter the days");
+scanf("%s",&a);
+if(a[0]=='s')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
