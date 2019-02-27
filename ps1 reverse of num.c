17012bc05970f203276345f82e2b4dba9e25096e
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-27 15:33:25 +0530

    Create ps1 reverse of num.c

---
+++ ps1 reverse of num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n,remainder,reverse=0;
+int result;
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+printf("\n%d",reverse);
+}
