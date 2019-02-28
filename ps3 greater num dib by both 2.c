26470beb7d3789658a61287ca5400c07363ee8e0
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-28 14:46:02 +0530

    Create ps3 greater num dib by both 2.c

---
+++ ps3 greater num dib by both 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n,k,i,len;
+	scanf("%d %d",&n,&k);
+	if(n>k)
+	{
+		len=n;
+	}
+	else
+	{
+		len=k;
+	}
+	for(i=len;i>=2;i--)
+	{
+		if(n%i==0 && k%i==0)
+		{
+			printf("%d",i);		
+		}
+	}
+}
