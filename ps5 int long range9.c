2e35bc7ae07c21a6b6a10ad5b742665fc564d219
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-02-28 17:01:39 +0530

    Create ps5 int long range9.c

---
+++ ps5 int long range9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	signed int n;
+	scanf("%d",&n);
+	if(n>=-32768 && n<=32767)
+	{
+		printf("INT");
+	}
+	else
+	{
+		printf("LONG LONG");
+	}
+	
+}
