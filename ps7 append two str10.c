ce7ae7430f37ad1d71cb9519cea9deca00058b9e
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-15 10:04:24 +0530

    Create ps7 append two str10.c

---
+++ ps7 append two str10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char str1[20],str2[20];
+	scanf("%s %s",str1,str2);
+	int i,j;
+	for(i=0;str1[i]!='\0';i++)
+	{
+		
+	}
+		for(j=0;str2[j]!='\0';j++,i++)
+		{
+			str1[i]=str2[j];
+		}
+	str1[i]='\0';
+	printf("%s",str1);
+	
+}
