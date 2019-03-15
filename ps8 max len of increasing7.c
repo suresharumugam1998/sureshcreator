459382735d8c383167391679a7f7d311e9b6e732
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-15 10:21:10 +0530

    Create ps8 max len of increasing7.c

---
+++ ps8 max len of increasing7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,count=1;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(array[i]<array[i+1])
+		{
+			count=count+1;
+		}
+		if(array[i]>array[i+1])
+		{
+			break;
+		}
+	}
+	printf("%d",count);
+	
+}
