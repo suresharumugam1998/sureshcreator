d7c57047707f7027ffc897826da95b5accfeb9ef
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-15 09:18:26 +0530

    Create ps6 kth smallest2.c

---
+++ ps6 kth smallest2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,k,array[50],i,j,temp;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(array[i]>array[j])
+		{
+			temp=array[i];
+			array[i]=array[j];
+			array[j]=temp;
+		}
+		}
+	}
+	printf("%d",array[k-1]);
+	
+	
+}
