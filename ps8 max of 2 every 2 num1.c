c191b894a0dada3f3de1e1c87fd14024810583e1
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-15 10:08:08 +0530

    Create ps8 max of 2 every 2 num1.c

---
+++ ps8 max of 2 every 2 num1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		if(array[i]>array[i+1])
+		{
+			printf("%d ",array[i]);
+		}
+		else
+		{
+			printf("%d ",array[i+1]);
+		}
+	}
+	
+}
