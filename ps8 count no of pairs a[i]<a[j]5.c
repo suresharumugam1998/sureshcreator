051c33302cb4ccabfc19f4b699ab5be07f732087
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-15 10:16:57 +0530

    Create ps8 count no of pairs a[i]<a[j]5.c

---
+++ ps8 count no of pairs a[i]<a[j]5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	int n,count=0;
+	scanf("%d",&n);
+	int array[n],j,i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(array[i]<array[j])
+			{
+				count=count+1;
+			}
+		}
+	}
+	printf("%d",count);
+	
+}
