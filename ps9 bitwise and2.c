8e491ea9f33c7c043f2bcfc77b8bce7018bcbd2f
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-21 16:34:26 +0530

    Create ps9 bitwise and2.c

---
+++ ps9 bitwise and2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],sum=-1,j,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<n;j++)
+    {
+        sum=((sum)&(a[j]));
+    }
+printf("%d",sum);
+getch();
+}
