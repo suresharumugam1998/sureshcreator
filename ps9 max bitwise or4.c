9707700eae7415b681629938e2faf8816a531f09
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-23 18:56:12 +0530

    Create ps9 max bitwise or4.c

---
+++ ps9 max bitwise or4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,max=0,res,j;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		res=(array[i])|(array[j]);
+		if(res>max)
+		{
+			max=res;
+		}
+		}
+	}
+	printf("%d",max);
+	
+}
