3fa66c3c5ba0c5e3ebd5f98adb8b0b22c9a85234
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-23 21:55:56 +0530

    Create ps11 sum of max min consecutive3.c

---
+++ ps11 sum of max min consecutive3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+   int n,a[100],i,s=0,t=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   t=a[0];
+   a[0]=a[n-1];
+   a[n-1]=t;
+   for(i=0;i<n-1;i++)
+   {
+       if(a[i]<a[i+1])
+       {
+           s=s+a[i+1];
+   }
+   else{
+   s=s+a[i];
+   }
+   }
+   printf("%d",s);
+}
