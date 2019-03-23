ad4ee085564263e4cc5411deb0b7ace4f5929429
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-23 18:32:52 +0530

    Create s10 sum ap 96.c

---
+++ s10 sum ap 96.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<conio.h>
+
+#include<math.h>
+
+void mian() 
+
+{
+
+	int n,a,d,s;
+
+	int N,D,A,S;
+
+	printf("\n enter the number of terms:");
+
+	scanf("%d",&n);
+
+	printf("\n enter the start value:");
+
+	scanf("%d",&a);
+
+	printf("\n enter the difference:");
+
+	scanf("%d",&d);
+
+	N=n-1;
+
+	D=N*d;
+
+	A=2*a;
+
+	S=A+D;
+
+	s=S*n/2;
+
+	printf("\n%d",s);
+
+
+
+}
