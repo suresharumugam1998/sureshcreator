137e0a22a47448fe2e906c1e7a1102110dd402ad
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-23 20:01:30 +0530

    Create ps8 n power k4.c

---
+++ ps8 n power k4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  int n,k=1,i,a[100],j;
+  scanf("%d",&j);
+   scanf("%d",&n);
+  for(i=0;i<j;i++)
+  {
+      k=k*n;
+  }
+  printf("%d",k);
+  
+}
