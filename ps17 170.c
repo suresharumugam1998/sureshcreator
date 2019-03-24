3faae335d819a2399ae0c57845f26694983a8aa7
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-24 23:40:35 +0530

    Create ps17 170.c

---
+++ ps17 170.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a[1000],b[100],s[100];
+    int i,n,j,l,c=1,max,k=0,p;
+    gets(a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        c=1;
+        for(j=i+1;j<l;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                a[j]='\0';
+            }
+        }
+        if(a[i]!='\0')
+        {
+            b[k]=a[i];
+            s[k]=c;
+            k++;
+            if(max<=c)
+            {
+                max=c;
+            }
+        }
+    }
+    printf("%d ",max);
+    for(i=0;i<k;i++)
+    {
+        if(s[i]==max)
+        {
+            printf("%c",b[i]);
+        }
+    }
+    
+    
+}
