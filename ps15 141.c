a00145e7791de9b9efddf5aa973906420cd3a2a4
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-03-25 00:34:01 +0530

    Create ps15 141.c

---
+++ ps15 141.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<conio.h>
+void main
+{
+    char a[1000];
+    int n,i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            printf("yes");
+            break;
+        }
+                   
+    }
+    if(c==0)
+    {
+        printf("no");
+    }
+
+    
+}
