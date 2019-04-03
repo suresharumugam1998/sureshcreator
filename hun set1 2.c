b9a89cc8136a65324b5f79e65d72c4c423609ec9
suresharumugam1998 <46838568+suresharumugam1998@users.noreply.github.com>
2019-04-03 16:01:36 +0530

    Create hun set1 2.c

---
+++ hun set1 2.c
@@ -0,0 +1,33 @@
+#include<conio.h>
+#include<stdio.h>
+int main(void)
+{
+int a[100],num,i,j,min,temp=0;
+long int sum=0,mul=1;
+clrscr();
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<num;i++)
+{
+for(j=i+1;j<num;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[j];
+a[j]=a[i];
+a[i]=temp;
+}
+}
+}
+for(i=0;i<num;i++)
+{
+sum=sum+a[i]*mul;
+mul=mul*10;
+}
+printf("%ld",sum);
+return 0;
+getch();
+}
